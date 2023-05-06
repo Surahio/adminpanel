00be6df7da0ae5c41c8ec8ea740988576d7152ee
Fateh <m.fateh.surahio@gmail.com>
2023-05-06 17:18:06 +0500

    subcomp-up
    majority done now only styling and small fixes maybe

--- src/SubComp.jsx
+++ src/SubComp.jsx
@@ -4,7 +4,6 @@ import {
   Container,
   Table,
   Button,
-  Modal,
   TextInput,
   Textarea,
 } from "@mantine/core";
@@ -34,7 +33,46 @@ const SubComp = ({ onUpdate, onDelete, onAdd }) => {
         "New element",
       ],
     },
-
+    {
+      title: "Basic Plan",
+      price: "$10/month",
+      features: ["1 GB Storage", "10 Users", "Email Support"],
+    },
+    {
+      title: "Pro Plan",
+      price: "$20/month",
+      features: ["10 GB Storage", "100 Users", "Email + Phone Support"],
+    },
+    {
+      title: "Enterprise Plan",
+      price: "$50/month",
+      features: [
+        "100 GB Storage",
+        "Unlimited Users",
+        "24/7 Support ",
+        "New element",
+      ],
+    },
+    {
+      title: "Basic Plan",
+      price: "$10/month",
+      features: ["1 GB Storage", "10 Users", "Email Support"],
+    },
+    {
+      title: "Pro Plan",
+      price: "$20/month",
+      features: ["10 GB Storage", "100 Users", "Email + Phone Support"],
+    },
+    {
+      title: "Enterprise Plan",
+      price: "$50/month",
+      features: [
+        "100 GB Storage",
+        "Unlimited Users",
+        "24/7 Support ",
+        "New element",
+      ],
+    },
   ];
 
   const [newPlan, setNewPlan] = useState({
@@ -42,7 +80,7 @@ const SubComp = ({ onUpdate, onDelete, onAdd }) => {
     price: "",
     features: [],
   });
-  const [showModal, setShowModal] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   const handleEdit = (index) => {
     setEditIndex(index);
@@ -63,18 +101,18 @@ const SubComp = ({ onUpdate, onDelete, onAdd }) => {
 
   const handleAdd = () => {
     setNewPlan({ title: "", price: "", features: [] });
-    setShowModal(true);
+    setShowForm(true);
   };
 
   const handleClose = () => {
-    setShowModal(false);
+    setShowForm(false);
   };
 
   const handleSaveNewPlan = () => {
     if (typeof onAdd === "function") {
       onAdd(newPlan);
     }
-    setShowModal(false);
+    setShowForm(false);
   };
 
   const handleTitleChange = (event) => {
@@ -103,283 +141,139 @@ const SubComp = ({ onUpdate, onDelete, onAdd }) => {
               />
             </a>
           </div>
-          <Container>
-            <Table className="sub-table">
-              <thead>
+          <div className="window">
+            <div className="Data-set">
+          <Table className="sub-table">
+             
                 <tr>
                   <th>Title</th>
                   <th>Price</th>
                   <th>Features</th>
                   <th>Actions</th>
                 </tr>
-              </thead>
+
+              
 
               {plans.map((plan, index) => (
-                <tr key={index}>
-                  <th>
-                    {editIndex === index ? (
-                      <input
-                        type="text"
-                        value={plan.title}
-                        onChange={(e) =>
-                          onUpdate(index, { ...plan, title: e.target.value })
-                        }
-                      />
-                    ) : (
-                      plan.title
-                    )}
-                  </th>
-                  <th>
-                    {editIndex === index ? (
-                      <input
-                        type="text"
-                        value={plan.price}
-                        onChange={(e) =>
-                          onUpdate(index, { ...plan, price: e.target.value })
-                        }
-                      />
-                    ) : (
-                      plan.price
-                    )}
-                  </th>
-                  <th>
-                    {editIndex === index ? (
-                      <Textarea
-                        value={plan.features.join("\n")}
-                        onChange={(e) =>
-                          onUpdate(index, {
-                            ...plan,
-                            features: e.target.value.split("\n"),
-                          })
-                        }
-                      />
-                    ) : (
-                      plan.features.map((feature, index) => (
-                        <div key={index}>{feature}</div>
-                      ))
-                    )}
-                  </th>
-                  <th>
-                    {editIndex === index ? (
-                      <>
-                        <Button
-                          variant="outline"
-                          color="blue"
-                          size="xs"
-                          onClick={() => handleSave(index, plan)}
-                        >
-                          Save
-                        </Button>{" "}
-                        <Button
-                          variant="outline"
-                          color="red"
-                          size="xs"
-                          onClick={handleCancel}
-                        >
-                          Cancel
-                        </Button>
-                      </>
-                    ) : (
-                      <>
-                        <Button
-                          variant="outline"
-                          color="gray"
-                     
-                          size="xs"
-                          onClick={() => handleEdit(index)}
-                        >
-                          Edit
-                        </Button>{" "}
-                        <Button
-                          variant="outline"
-                          color="red"
-                          size="xs"
-                          onClick={() => handleDelete(index)}
-                        >
-                          Delete
-                        </Button>
-                      </>
-                    )}
-                  </th>
-                </tr>
-              ))}
-            </Table>
-            <Button
-              style={{ marginTop: 20 }}
-              onClick={handleAdd}
-              variant="outline"
-              color="blue"
-            >
-              Add new plan
-            </Button>
 
-            <Modal opened={showModal} onClose={handleClose} title="New Plan">
-              <TextInput
-                label="Title"
-                placeholder="Enter title"
-                value={newPlan.title}
-                onChange={handleTitleChange}
-              />
-              <br />
-              <TextInput
-                label="Price"
-                placeholder="Enter price"
-                value={newPlan.price}
-                onChange={handlePriceChange}
-              />
-              <br />
-              <Textarea
-                label="Features"
-                placeholder="Enter features (one per line)"
-                value={newPlan.features.join("\n")}
-                onChange={handleFeaturesChange}
-              />
-              <br />
-              <Button onClick={handleSaveNewPlan}>Save</Button>
-            </Modal>
-          </Container>
-        </div>
+  <tr key={index}>
+    <th>
+      {editIndex === index ? (
+        <TextInput
+          value={plan.title}
+          onChange={(event) =>
+            handleSave(index, { ...plan, title: event.target.value })
+          }
+        />
+      ) : (
+        plan.title
+      )}
+    </th>
+    <th>
+      {editIndex === index ? (
+        <TextInput
+          value={plan.price}
+          onChange={(event) =>
+            handleSave(index, { ...plan, price: event.target.value })
+          }
+        />
+      ) : (
+        plan.price
+      )}
+    </th>
+    <th>
+      {editIndex === index ? (
+        <Textarea
+          value={plan.features.join("\n")}
+          onChange={(event) =>
+            handleSave(index, {
+              ...plan,
+              features: event.target.value.split("\n"),
+            })
+          }
+        />
+      ) : (
+        plan.features.join(", ")
+      )}
+    </th>
+    <th>
+      {editIndex === index ? (
+        <>
+          <Button
+            variant="light"
+            onClick={() => handleSave(index, plan)}
+            style={{ marginRight: "8px" }}
+          >
+            Save
+          </Button>
+          <Button variant="light" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </>
+      ) : (
+        <>
+          <Button
+            variant="light"
+            onClick={() => handleEdit(index)}
+            style={{ marginRight: "8px" }}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="light"
+            onClick={() => handleDelete(index)}
+            style={{ marginRight: "8px" }}
+          >
+            Delete
+          </Button>
+        </>
+      )}
+    </th>
+  </tr>
+))}
+</Table>
+
+</div>
+<Button variant="light" onClick={handleAdd}>
+  Add New Plan
+</Button>
+</div>
+
+
+{showForm && (
+  <div className="form-overlay">
+    <div className="form-container">
+      <h2>Add New Plan</h2>
+      <TextInput
+        label="Title"
+        value={newPlan.title}
+        onChange={handleTitleChange}
+      />
+      <TextInput
+        label="Price"
+        value={newPlan.price}
+        onChange={handlePriceChange}
+      />
+      <Textarea
+        label="Features"
+        placeholder="Enter one feature per line"
+        value={newPlan.features.join("\n")}
+        onChange={handleFeaturesChange}
+      />
+      <div className="button-container">
+        <Button variant="light" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="light" onClick={handleSaveNewPlan}>
+          Save
+        </Button>
       </div>
-    </>
-  );
+    </div>
+  </div>
+)}
+</div>
+</div>
+</>
+);
 };
-
 export default SubComp;
-
-// import React, { useState } from "react";
-// import "./style.css";
-// import { Container, Table, Button } from "@mantine/core";
-// import Sidebar from "./Sidebar.jsx";
-// import "./home.css";
-
-// const SubComp = ({  onUpdate, onDelete, onAdd }) => {
-//   const [editIndex, setEditIndex] = useState(-1);
-//   const plans=[
-//     {
-//       title: "Basic Plan",
-//       price: "$10/month",
-//       features: ["1 GB Storage", "10 Users", "Email Support"],
-//     },
-//     {
-//       title: "Pro Plan",
-//       price: "$20/month",
-//       features: ["10 GB Storage", "100 Users", "Email + Phone Support"],
-//     },
-//     {
-//       title: "Enterprise Plan",
-//       price: "$50/month",
-//       features: [
-//         "100 GB Storage",
-//         "Unlimited Users",
-//         "24/7 Support ",
-//         "New element",
-//       ],
-//     },
-//   ];
-//   const [newPlan, setNewPlan] = useState({ title: "", price: "", features: [] });
-
-//   const handleEdit = (index) => {
-//     setEditIndex(index);
-//   };
-
-//   const handleSave = (index, updatedPlan) => {
-//     onUpdate(index, updatedPlan);
-//     setEditIndex(-1);
-//   };
-
-//   const handleCancel = () => {
-//     setEditIndex(-1);
-//   };
-
-//   const handleDelete = (index) => {
-//     onDelete(index);
-//   };
-
-//   const handleAdd = () => {
-//     setEditIndex(plans.length);
-//     if (typeof onAdd === 'function') {
-//       onAdd(newPlan);
-//     }
-//     setNewPlan({ title: "", price: "", features: [] });
-//   };
-
-//   return (
-//     <>
-//     <div className="container-new">
-//           <Sidebar/>
-//           <div className="others">
-//           <div className="row">
-//       <a href="" target="_blank">
-//         <img src="/Logo_Ezhire.svg" className="logo tauri" alt="Tauri logo" />
-//       </a>
-//     </div><Container>
-//         <Table>
-//           <thead>
-//             <tr>
-//               <th>Title</th>
-//               <th>Price</th>
-//               <th>Features</th>
-//               <th>Actions</th>
-//             </tr>
-//           </thead>
-
-//           {plans.map((plan, index) => (
-//             <tr key={index}>
-//               <th>
-//                 {editIndex === index ? (
-//                   <input
-//                     type="text"
-//                     value={plan.title}
-//                     onChange={(e) => onUpdate(index, { ...plan, title: e.target.value })} />
-//                 ) : (
-//                   plan.title
-//                 )}
-//               </th>
-//               <th>
-//                 {editIndex === index ? (
-//                   <input
-//                     type="text"
-//                     value={plan.price}
-//                     onChange={(e) => onUpdate(index, { ...plan, price: e.target.value })} />
-//                 ) : (
-//                   plan.price
-//                 )}
-//               </th>
-//               <th>
-//                 {editIndex === index ? (
-//                   <textarea
-//                     value={plan.features.join("\n")}
-//                     onChange={(e) => onUpdate(index, {
-//                       ...plan,
-//                       features: e.target.value.split("\n"),
-//                     })} />
-//                 ) : (
-//                   plan.features.join(", ")
-//                 )}
-//               </th>
-//               <th>
-//                 {editIndex === index ? (
-//                   <>
-//                     <Button onClick={() => handleSave(index, plan)}>Save</Button>{" "}
-//                     <Button onClick={handleCancel}>Cancel</Button>
-//                   </>
-//                 ) : (
-//                   <>
-//                     <Button onClick={() => handleEdit(index)}>Edit</Button>{" "}
-//                     <Button onClick={() => handleDelete(index)}>Delete</Button>
-//                   </>
-//                 )}
-//               </th>
-//             </tr>
-//           ))}
-
-//         </Table>
-//         <Button onClick={handleAdd}>Add</Button>
-//       </Container>
-//           </div>
-//     </div>
-//     </>
-//   );
-// };
-
-// export default SubComp;
