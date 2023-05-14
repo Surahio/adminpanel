4190b9efca97a0d6a14c5fbd1f6504b11d2505e2
Fateh <m.fateh.surahio@gmail.com>
2023-05-14 13:57:50 +0500

    table-view_added

--- src/UserStatus.jsx
+++ src/UserStatus.jsx
@@ -8,10 +8,16 @@ function UserStatus() {
   const [isBlocked, setIsBlocked] = useState(false);
   const [userId, setUserId] = useState('');
   const [userName, setUserName] = useState('');
+  const [users, setUsers] = useState([
+    { id: '1', name: 'John Doe', status: 'active' },
+    { id: '2', name: 'Jane Smith', status: 'blocked' },
+    { id: '3', name: 'Bob Johnson', status: 'active' },
+  ]);
+  const [searchQuery, setSearchQuery] = useState('');
 
-const handleIdChange = (event) => {
+  const handleIdChange = (event) => {
     setUserId(event.target.value);
- };
+  };
 
   const handleBlock = () => {
     setIsBlocked(true);
@@ -24,12 +30,6 @@ const handleIdChange = (event) => {
   };
 
   const handleCheckStatus = () => {
-    // for now check on dummy dat but haf to add function for db
-    const users = [
-      { id: '1', name: 'John Doe', status: 'active' },
-      { id: '2', name: 'Jane Smith', status: 'blocked' },
-      { id: '3', name: 'Bob Johnson', status: 'active' },
-    ];
     const user = users.find((user) => user.id === userId);
     if (user) {
       setUserName(user.name);
@@ -42,38 +42,75 @@ const handleIdChange = (event) => {
     }
   };
 
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
-    <div className="container-new">
-          <Sidebar/>
-          <div className="others">
+      <div className="container-new">
+        <Sidebar />
+        <div className="others">
           <div className="row">
-          <a href="" target="_blank">
+            <a href="" target="_blank">
               <img src="/Logo_Ezhire.svg" className="logo tauri" alt="Tauri logo" />
-          </a>
-      </div><div className='status'>
-              <label htmlFor="user-id">User ID</label>
-              <br></br>
-              <input type="text" id="user-id" value={userId} onChange={handleIdChange} />
-              <br></br>
-              <button onClick={handleCheckStatus}>Check Status</button>
-              {status && (
-                  <div>
-                      <p>User name: {userName}</p>
-                      <p>User status: {status}</p>
-                      <p>User is {isBlocked ? 'blocked' : 'unblocked'}</p>
-                      {!isBlocked ? (
-                          <button onClick={handleBlock}>Block User</button>
-                      ) : (
-                          <button onClick={handleUnblock}>Unblock User</button>
-                      )}
-                  </div>
-              )}
+            </a>
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Search by name" value={searchQuery} onChange={handleSearch} />
+          </div>
+          
+          <div className="user-list">
+            <h2>User List</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user) => (
+                  <tr key={user.id}>
+                    <th>{user.id}</th>
+                    <th>{user.name}</th>
+                    <th>{user.status}</th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className='status'>
+            <label htmlFor="user-id">User ID</label>
+            <br />
+            <input type="text" id="user-id" value={userId} onChange={handleIdChange} />
+            <br />
+            <button onClick={handleCheckStatus}>Check Status</button>
+            {status && (
+              <div>
+                <p>User name: {userName}</p>
+                <p>User status: {status}</p>
+                <p>User is {isBlocked ? 'blocked' : 'unblocked'}</p>
+                {!isBlocked ? (
+                  <button onClick={handleBlock}>Block User</button>
+                ) : (
+                  <button onClick={handleUnblock}>Unblock User</button>
+                )}
+              </div>
+            )}
           </div>
           </div>
-    </div>
+        </div>
+      </div>
+      
     </>
   );
 }
 
-export default UserStatus;
+export default  UserStatus
+
+
